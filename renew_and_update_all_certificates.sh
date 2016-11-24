aa05d88df957d41b402a1b688388704ddd6f36ab
Dan Stutzman <dan@danstutzman.com>
2016-11-24 10:55:20 -0700

    Clarify script to hide/ignore expected DeleteConflict error.

--- renew_and_update_all_certificates.sh
+++ renew_and_update_all_certificates.sh
@@ -1,7 +1,20 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
+# Try to delete all server certificates.  The ones that are in use will give a
+# DeleteConflict message, which we can ignore
+echo "aws iam list-server-certificates"
 for CERT_NAME in `aws iam list-server-certificates | python -c "import json, sys; print '\n'.join([cert['ServerCertificateName'] for cert in json.load(sys.stdin)['ServerCertificateMetadataList']])"`; do
-  aws iam delete-server-certificate --server-certificate-name $CERT_NAME || true
+  echo "aws iam delete-server-certificate --server-certificate-name $CERT_NAME"
+  set +e
+  OUTPUT=`aws iam delete-server-certificate --server-certificate-name $CERT_NAME 2>&1`
+  ERROR=$?
+  set -e
+  if [ $ERROR != 0 ]; then
+    case "$OUTPUT" in
+      *DeleteConflict*) ;; # It won't let us delete certificates that are in use
+      *) echo 1>&2 "$ERROR from delete-server-certificate: $OUTPUT"; exit 1;;
+    esac
+  fi
 done
 
 ./renew_certificate.sh vocabincontext.com
